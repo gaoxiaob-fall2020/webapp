0bb102c545de955f10601c692f73e29f12998464
Xiaobin-Gao <gao.xiaob@northeastern.edu>
2020-11-10 22:14:56 -0500

    fix

--- codedeploy/af_install.sh
+++ codedeploy/af_install.sh
@@ -9,3 +9,6 @@ sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl \
     -m ec2 \
     -c file:/opt/aws/amazon-cloudwatch-agent/etc/amazon-cloudwatch-agent.json \
     -s
+
+sudo touch /opt/aws/amazon-cloudwatch-agent/logs/webapp.log
+sudo chown ubuntu:ubuntu /opt/aws/amazon-cloudwatch-agent/logs/webapp.log
