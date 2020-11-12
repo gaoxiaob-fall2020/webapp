c0bbe0fb7628b5a4c29d34bdfbd46f6fefcf1deb
Xiaobin-Gao <gao.xiaob@northeastern.edu>
2020-11-12 00:04:49 -0500

    fix port

--- codedeploy/b4_install.sh
+++ codedeploy/b4_install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # Remove previous artifact
-if [ -d /home/ubuntu/app ]; then sudo rm -rf /home/ubuntu/app; fi 
-if [ -d /home/ubuntu/cloudwatch ]; then sudo rm -rf /home/ubuntu/cloudwatch; fi 
-if [ -f /etc/nginx/sites-available/dp_ng.conf ]; then sudo rm -f /etc/nginx/sites-available/dp_ng.conf; fi
+sudo rm -rf /home/ubuntu/app
+sudo rm -rf /home/ubuntu/cloudwatch
+sudo rm -f /etc/nginx/sites-available/dp_ng.conf
