325e64a3c7d79838a60960e83ef6a45ae3b3a081
Xiaobin-Gao <gao.xiaob@northeastern.edu>
2020-11-11 23:17:30 -0500

    fix

--- codedeploy/b4_install.sh
+++ codedeploy/b4_install.sh
@@ -3,3 +3,4 @@
 # Remove previous artifact
 if [ -d /home/ubuntu/app ]; then sudo rm -rf /home/ubuntu/app; fi 
 if [ -d /home/ubuntu/cloudwatch ]; then sudo rm -rf /home/ubuntu/cloudwatch; fi 
+if [ -f /etc/nginx/sites-available/dp_ng.conf ]; then sudo rm -f /etc/nginx/sites-available/dp_ng.conf; fi
